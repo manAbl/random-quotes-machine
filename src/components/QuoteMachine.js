39f0eac0e03186e567025a5ee93fe9a99e66a170
manuel blanco <manuel.22.lamas@gmail.com>
2018-03-28 14:29:19 -0400

    Removed components and left just one also added the final layout

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import Button from './Button';
-import Quotes from './Quotes.js';
 import '.././styles/quotemachine-styles.css';
 
 class QuoteMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote:"Hello There"
+      quote:"Hello There",
+      author:"Nunito"
     }
     this.handleClick = this.handleClick.bind(this);
   }
-
   handleClick() {
     this.setState({
       quote: "Bye Bye"
     })
   }
-
   render() {
     return(
       <div className="app-wrapper">
-        <h1>Random Quote Machine</h1>
-        <hr className="star"/>
-        <p> Serving you fantastic quotes that inspire people since 2018</p>
-        <Button onClick={this.handleClick} />
-        <Quotes quote={this.state.quote} />
+        <blockquote>
+          <p>{this.state.quote}</p>
+          <label htmlFor="author">- <span>{this.state.author}</span></label>
+        </blockquote>
+        <button onClick={this.handleClick}>Get Random</button>
       </div>
       )
   }
