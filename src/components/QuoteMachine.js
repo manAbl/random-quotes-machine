d10bc42d0e33ae25359de504b671e0bf54752da0
manuel blanco <manuel.22.lamas@gmail.com>
2018-03-28 17:06:20 -0400

    Fetched the data and saved to the state also added the twtiiter button to retweet each quote

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import '.././styles/quotemachine-styles.css';
 
+const API = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+
 class QuoteMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote:"Hello There",
-      author:"Nunito"
+      quotes:[]
     }
     this.handleClick = this.handleClick.bind(this);
   }
+  componentDidMount() {
+    fetch(API).then(response => response.json()).then(data => this.setState({ quotes: data }));
+  }
   handleClick() {
     this.setState({
-      quote: "Bye Bye"
+      quotes: "Bye Bye"
     })
   }
   render() {
@@ -20,8 +24,9 @@ class QuoteMachine extends React.Component {
       <div className="app-wrapper">
         <blockquote>
           <p>{this.state.quote}</p>
-          <label htmlFor="author">- <span>{this.state.author}</span></label>
+          <label htmlFor="author">-{this.state.title} <span></span></label>
         </blockquote>
+        <i className="twitter fab fa-twitter"></i>
         <button onClick={this.handleClick}>Get Random</button>
       </div>
       )
