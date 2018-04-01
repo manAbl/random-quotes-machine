4a983f2a7d49d6206c34cbd98273aec7a404867e
manuel blanco <manuel.22.lamas@gmail.com>
2018-04-01 19:59:29 -0400

    Change it where I make the API call and fixed styles for rwd

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,35 +1,80 @@
 import React from 'react';
 import '.././styles/quotemachine-styles.css';
 
-const API = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+const API = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10';
+
+const myHeaders = new Headers({
+  "X-Mashape-Key": "jF4KXIiieQmshA5TsCPoKY4Jx8DJp1EYqivjsnfil86uJbD0IM",
+  "Content-Type": "application/x-www-form-urlencoded",
+  "Accept": "application/json"
+});
+
+const body = {
+  method: 'POST',
+  headers: myHeaders,
+  cache: 'false'
+};
+
+const myRequest = new Request(API, body);
 
 class QuoteMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes:[],
+      quotes: [],
+      loading: false,
+      error: null,
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.fetchingQuotes = this.fetchingQuotes.bind(this);
+  }
+  fetchingQuotes () {
+    fetch(myRequest).then(
+      response => response.json()).then(
+        data => this.setState({ quotes: data , loading: false })).catch(
+          error => this.setState({
+            error: error,
+            loading: false }));
   }
   componentDidMount() {
-    fetch(API).then(response => response.json()).then(data => this.setState({quotes: data}));
-  }
+    this.setState({
+      loading: true,
+    })
 
-  /*componentDidMount() {
-    const { quotes } = this.state;
-    let number = Math.floor(Math.random() * Math.random());
-  }*/
+   this.fetchingQuotes();
+  }
+  /*tweetQuote() {
+    let twitterURL = 'https://twitter.com/intent/tweet?hashtags=quotes,freeCodeCamp&related=freecodecamp&text="';
+    let quote = document.getElementById('quote').String();
+    let author = document.getElementById('author').String();
+       twitterURL +=quote +'" - '+ author;
+      return twitterURL;
+   }*/
+  handleClick() {
+    this.fetchingQuotes();
+  }
   render() {
-    const { quotes } = this.state;
+    const { error, quotes, loading } = this.state;
+    const blockquote = error ? (
+      <blockquote>
+        Something went wrong ... :(
+      </blockquote>
+      ) : ( loading ? (
+      <blockquote>
+        Loading ...
+      </blockquote>
+      ) : (
+      <blockquote>
+        <i className="fas fa-quote-right"></i> <p id="quote">{ quotes.quote }</p>
+        <label id="author"htmlFor="author">- <span> { quotes.author } </span></label>
+      </blockquote>
+      ));
+
     return (
       <div className="app-wrapper">
-        { quotes.map( quote =>
-          <blockquote>
-            {quote.content}
-            <label htmlFor="author">-  <span>{quote.title}</span></label>
-          </blockquote>
-        )}
-        <i className="twitter fab fa-twitter"></i>
-        <button >Get Random</button>
+          { blockquote }
+        <button className="twitter-btn"><i className="twitter fab fa-twitter"></i></button>
+        <button className="random-btn" onClick={this.handleClick}>Get Random <i className="fas fa-random"></i></button>
       </div>
       )
   }
